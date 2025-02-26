8f4a8dfbcf460c9568cc8e38a0e40fd1565b9ad1
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-02-26 16:59:59 -0500

    creates movie and genre model, registers movies, genre, and users to admin.py

--- backend/movierental/movies/models.py
+++ backend/movierental/movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)  # relation to Genre model
+    description = models.TextField(blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True)
+    available_copies = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
