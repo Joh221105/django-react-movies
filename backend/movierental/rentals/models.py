ed401eb4bd1c541c2846e18859dbc72f55da0d18
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-02-26 17:07:23 -0500

    defines rental model, fixes user admin.py register bug, registers rental in admin.py, ran migrations

--- backend/movierental/rentals/models.py
+++ backend/movierental/rentals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from movies.models import Movie
 
-# Create your models here.
+class Rental(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rented_at = models.DateTimeField(auto_now_add=True)
+    returned_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} rented {self.movie.title}"
