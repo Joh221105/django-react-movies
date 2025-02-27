52f68a6fc8a4cd7cba85f8b8550752dc6f8e5730
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-02-27 16:19:47 -0500

    implements get all movies and get movie by id function in movie views.py

--- backend/movierental/movies/views.py
+++ backend/movierental/movies/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from .models import Movie
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+# get all movies
+
+def get_all_movies(request):
+    if request.method == "GET":
+        movies = list(Movie.objects.values(
+            "id", "title", "genre_name", "description", "release_date", "available_copies"
+        ))
+        return JsonResponse(movies, safe=False)
+    return JsonResponse({"error": "Invalid request"}, status=405)
+
+
+
+# get movie by id
+
+def get_movie_by_id(request, movie_id):
+    if request.method == "GET":
+        try:
+            movie = Movie.objects.get(id=movie_id)
+            return JsonResponse({
+                "id": movie.id,
+                "title": movie.title,
+                "genre": movie.genre.name if movie.genre else None,
+                "description": movie.description,
+                "release_date": movie.release_date,
+                "available_copies": movie.available_copies
+            })
+        except Movie.DoesNotExist:
+            return JsonResponse({"error": "Movie not found"}, status=404)
+    return JsonResponse({"error": "Invalid request"}, status=405)
+
+# create movie
+
+# update movie
+
+# delete movie
+
+# filter movie by genre
